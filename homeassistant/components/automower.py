62d44b65076c1bd5bc5c5511fb31d9d4da1fc093
Jan Schulz-Hofen <jan@plan.io>
2017-09-03 13:23:18 +0200

    big refactoring

--- homeassistant/components/automower.py
+++ homeassistant/components/automower.py
@@ -9,21 +9,38 @@ import copy
 import logging
 import voluptuous as vol
 
-from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_SCAN_INTERVAL
+from homeassistant.const import CONF_ICON, CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_USERNAME
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers import discovery
 from homeassistant.helpers.entity import Entity
+from homeassistant.util import slugify
 
 _LOGGER = logging.getLogger(__name__)
 
 REQUIREMENTS = ['pyhusmow==0.1']
-
 DOMAIN = 'automower'
-
-AUTOMOWER_API_CLIENT_HANDLE = 'automower'
-
-ICON = 'mdi:robot'
+DEFAULT_ICON = 'mdi:robot'
 VENDOR = 'Husqvarna'
+
+# Add more states
+STATE_ICONS = {
+    'ERROR': 'mdi:alert'
+}
+
+# TODO: Add more models
+MODELS = {
+    'H': 'Automower 450X'
+}
+
+IGNORED_API_STATE_ATTRIBUTES = [
+    'cachedSettingsUUID',
+    'lastLocations',
+    'valueFound'
+]
+
+AUTOMOWER_COMPONENTS = [
+    'sensor', 'device_tracker'
+]
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -31,25 +48,19 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_PASSWORD): cv.string,
         vol.Optional(CONF_SCAN_INTERVAL, default=30):
             vol.All(cv.positive_int, vol.Clamp(min=30)),
-
     }),
 }, extra=vol.ALLOW_EXTRA)
 
-AUTOMOWER_COMPONENTS = [
-    'sensor', 'device_tracker'
-]
-
-
 def setup(hass, base_config):
     """Establish connection to Husqvarna Automower API."""
-    from pyhusmow import API
+    from pyhusmow import API as HUSMOW_API
 
     config = base_config.get(DOMAIN)
 
     if hass.data.get(DOMAIN) is None:
         hass.data[DOMAIN] = { 'devices': [] }
 
-    api = API()
+    api = HUSMOW_API()
     api.login(config.get(CONF_USERNAME), config.get(CONF_PASSWORD))
 
     robots = api.list_robots()
@@ -58,9 +69,7 @@ def setup(hass, base_config):
         return False
 
     for robot in robots:
-        device_api = copy.copy(api)
-        device_api.select_robot(robot['id'])
-        hass.data[DOMAIN]['devices'].append({'meta': robot, 'api': device_api})
+        hass.data[DOMAIN]['devices'].append(AutomowerDevice(robot, api))
 
     for component in AUTOMOWER_COMPONENTS:
         discovery.load_platform(hass, component, DOMAIN, {}, base_config)
@@ -71,18 +80,27 @@ def setup(hass, base_config):
 class AutomowerDevice(Entity):
     """Representation of an Automower device."""
 
-    def __init__(self, id, name, model, api):
+    def __init__(self, meta, api):
         """Initialisation of the Automower device."""
-        self._id = id
-        self._name = name
-        self._model = model
+        _LOGGER.debug("Initializing Automower device: %s", meta['name'])
+        self._id = meta['id']
+        self._name = meta['name']
+        self._model = meta['model']
         self._state = None
-        self._api = api
+        self._see = None
+
+        # select robot in api
+        self._api = copy.copy(api)
+        self._api.select_robot(self._id)
 
     @property
     def id(self):
         """Return the id of the Automower."""
         return self._id
+
+    @property
+    def dev_id(self):
+        return slugify("{0}_{1}_{2}".format(DOMAIN, self._model, self._id))
 
     @property
     def name(self):
@@ -92,19 +110,62 @@ class AutomowerDevice(Entity):
     @property
     def model(self):
         """Return the model of the Automower."""
-        return self._model
+        return MODELS.get(self._model,self._model)
 
     @property
     def icon(self):
         """Return the icon for the frontend."""
-        return ICON
+        return STATE_ICONS.get(self.state, DEFAULT_ICON)
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state['mowerStatus']
+
+    @property
+    def battery(self):
+        return self._state['batteryPercent']
+
+    @property
+    def lat(self):
+        return self._state['lastLocations'][0]['latitude']
+
+    @property
+    def lon(self):
+        return self._state['lastLocations'][0]['longitude']
+
+    @property
+    def battery(self):
+        return self._state['batteryPercent']
 
     @property
     def device_state_attributes(self):
         """Return the state attributes of the device."""
-        return self._state
+        # remove attributes that are exposed via properties or irrelevant
+        return { k: v for k, v in self._state.items() if not k in IGNORED_API_STATE_ATTRIBUTES }
+
+    def set_see(self, see):
+        self._see = see
 
     def update(self):
         """Update the state from the sensor."""
         _LOGGER.debug("Updating sensor: %s", self._name)
         self._state = self._api.status()
+        if self._see is not None:
+            self.update_see()
+
+    def update_see(self):
+        """Update the device tracker."""
+        _LOGGER.debug("Updating device tracker: %s", self._name)
+        self._see(
+            dev_id=self.dev_id,
+            host_name=self.name,
+            battery=self.battery,
+            gps=(self.lat, self.lon),
+            attributes={
+                'status': self.state,
+                'id': self.dev_id,
+                'name': self.name,
+                CONF_ICON: self.icon,
+                'vendor': VENDOR,
+                'model': self.model})
--- homeassistant/components/sensor/automower.py
+++ homeassistant/components/sensor/automower.py
@@ -4,46 +4,11 @@ Sensors for Husqvarna Automowers.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.automower/
 """
-import logging
-from datetime import timedelta
 
-from homeassistant.const import ATTR_BATTERY_LEVEL, CONF_STATE
-from homeassistant.components.sensor import ENTITY_ID_FORMAT
-from homeassistant.components.automower import DOMAIN as AUTOMOWER_DOMAIN, AutomowerDevice
-from homeassistant.helpers.entity import Entity
-
-_LOGGER = logging.getLogger(__name__)
+from homeassistant.components.automower import DOMAIN as AUTOMOWER_DOMAIN
 
 DEPENDENCIES = ['automower']
 
-SCAN_INTERVAL = timedelta(seconds=30)
-
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Husqvarna Automower sensor platform."""
-    devices = []
-
-    for device in hass.data[AUTOMOWER_DOMAIN]['devices']:
-        devices.append(
-            AutomowerSensor(
-                device['meta']['id'],
-                device['meta']['name'],
-                device['meta']['model'],
-                device['api']
-            )
-        )
-
-    add_devices(devices, True)
-
-
-class AutomowerSensor(AutomowerDevice, Entity):
-    """Representation of Automower sensors."""
-
-    def __init__(self, id, name, model, api, sensor_type=None):
-        """Initialisation of the sensor."""
-        super().__init__(id, name, model, api)
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state['mowerStatus']
+    add_devices(hass.data[AUTOMOWER_DOMAIN]['devices'], True)
